45355905faa96c518481815bbac578969cd7b7ca
Carlos <carlosaldaravi@gmail.com>
2020-04-27 23:49:47 +0200

    fix read user dto

--- src/modules/user/dto/read-user.dto.ts
+++ src/modules/user/dto/read-user.dto.ts
@@ -1,14 +1,23 @@
 import { IsNumber, IsEmail } from 'class-validator';
 import { ReadUserDetailsDto } from './read-user-details.dto';
-import { Type } from 'class-transformer';
+import { Type, Exclude, Expose } from 'class-transformer';
+import { ReadRoleDto } from '../../role/dtos';
 
+@Exclude()
 export class ReadUserDto {
+  @Expose()
   @IsNumber()
   readonly id: number;
 
+  @Expose()
   @IsEmail()
   readonly email: string;
 
+  @Expose()
   @Type(type => ReadUserDetailsDto)
   readonly details: ReadUserDetailsDto;
+
+  @Expose()
+  @Type(type => ReadRoleDto)
+  readonly roles: ReadRoleDto[];
 }
