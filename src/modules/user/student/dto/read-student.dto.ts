924048ac056c8b6a2feb1747001f9f3a64b56779
Carlos <carlosaldaravi@gmail.com>
2020-04-29 18:23:19 +0200

    [ADD] read student dto

---
+++ src/modules/user/student/dto/read-student.dto.ts
@@ -0,0 +1,23 @@
+import { IsNumber, IsEmail } from 'class-validator';
+import { Type, Exclude, Expose } from 'class-transformer';
+import { ReadRoleDto } from '../../../role/dtos';
+import { ReadStudentDetailsDto } from './read-student-detail.dto';
+
+@Exclude()
+export class ReadStudentDto {
+  @Expose()
+  @IsNumber()
+  readonly id: number;
+
+  @Expose()
+  @IsEmail()
+  readonly email: string;
+
+  @Expose()
+  @Type(type => ReadStudentDetailsDto)
+  readonly details: ReadStudentDetailsDto;
+
+  @Expose()
+  @Type(type => ReadRoleDto)
+  readonly roles: ReadRoleDto[];
+}
