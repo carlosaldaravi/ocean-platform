ac3ab351e3742c28ff0a7ca6297b9d567d4e11c3
Carlos <carlosaldaravi@gmail.com>
2020-04-29 02:12:37 +0200

    [FIX] user decorator

--- src/modules/auth/user.decorator.ts
+++ src/modules/auth/user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator } from '@nestjs/common';
 import { UserDto } from '../user/dto/user.dto';
 
-export const GetUser = createParamDecorator((key, req) =>
-  key ? req.user[key] : req.user,
-);
+export const GetUser = createParamDecorator((key, req) => {
+  return key ? req.args[0].user[key] : req.args[0].user;
+});
