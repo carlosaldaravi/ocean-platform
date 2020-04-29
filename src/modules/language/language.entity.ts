b125ec9b545236202647eb36cf9250ee701d839e
Carlos <carlosaldaravi@gmail.com>
2020-04-29 02:13:20 +0200

    [ADD] language entity

---
+++ src/modules/language/language.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+import { status } from '../../shared/entity-status.enum';
+
+@Entity('languages')
+export class Language extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 20, nullable: false, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', default: status.ACTIVE, length: 8 })
+  status: string;
+
+  // @ManyToOne(
+  //   type => User,
+  //   user => user.date,
+  // )
+  // @JoinColumn({ name: 'user_id' })
+  // user: User;
+}
