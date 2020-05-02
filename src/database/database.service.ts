18faeef8a5920b6c79c689a34d4cf1f80c75ff9e
Carlos <carlosaldaravi@gmail.com>
2020-05-02 18:05:09 +0200

    [IMP] update logging error

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -17,7 +17,8 @@ export const databaseProviders = [
         database: config.get(Configuration.DATABASE),
         username: config.get(Configuration.USERNAME),
         password: config.get(Configuration.PASSWORD),
-        logging: false,
+        synchronize: false,
+        logging: ['error'],
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migrations/{.ts,.js}'],
       } as ConnectionOptions;
