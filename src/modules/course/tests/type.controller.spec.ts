6fa6a88fe69d125e8ce7a01a241d45341e20e60a
Carlos <carlosaldaravi@gmail.com>
2020-05-03 05:02:02 +0200

    [ADD] course type spec

---
+++ src/modules/course/tests/type.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CourseTypeController } from '../type/course-type.controller';
+
+describe('CourseType Controller', () => {
+  let controller: CourseTypeController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CourseTypeController],
+    }).compile();
+
+    controller = module.get<CourseTypeController>(CourseTypeController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
