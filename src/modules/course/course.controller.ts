bcbd1d85b6d1d2b8696be58c0575c3b84610fdf3
Carlos <carlosaldaravi@gmail.com>
2020-05-03 05:06:02 +0200

    [ADD] create course , set paid student and cashed instructor

--- src/modules/course/course.controller.ts
+++ src/modules/course/course.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
+import { CourseService } from './course.service';
+import { RoleGuard } from '../role/guards/role.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { RoleType } from '../role/roletype.enum';
+import { Roles } from '../role/decorators/role.decoratos';
+import { ReadCourseDto, CreateCourseDto } from './dto';
+import { CreateCourseCalendarDto } from '../calendar/dto';
 
-@Controller('course')
-export class CourseController {}
+@Controller('courses')
+@UseGuards(AuthGuard('jwt'), RoleGuard)
+export class CourseController {
+  constructor(private _courseService: CourseService) {}
+
+  @Get(':courseId')
+  getCourse(
+    @Param('courseId', ParseIntPipe) courseId: number,
+  ): Promise<ReadCourseDto> {
+    return this._courseService.get(courseId);
+  }
+
+  @Get()
+  async getCourses(): Promise<ReadCourseDto[]> {
+    return this._courseService.getAll();
+  }
+
+  @Post()
+  @Roles(RoleType.ADMIN)
+  createCourse(@Body() body: any): Promise<ReadCourseDto> {
+    const course: Partial<CreateCourseDto> = body.course;
+    const calendar: Partial<CreateCourseCalendarDto> = body.calendar;
+    return this._courseService.create(course, calendar);
+  }
+
+  @Patch('paid/:courseId/:studentId')
+  studentPaid(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('studentId', ParseIntPipe) studentId: number,
+  ): Promise<ReadCourseDto> {
+    return this._courseService.studentPaid(courseId, studentId);
+  }
+
+  @Patch('cashed/:courseId/:instructorId')
+  instructorCashed(
+    @Param('courseId', ParseIntPipe) courseId: number,
+    @Param('instructorId', ParseIntPipe) instructorId: number,
+  ): Promise<ReadCourseDto> {
+    return this._courseService.instructorCashed(courseId, instructorId);
+  }
+}
--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -1,4 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CourseRepository } from '../course/course.reposity';
+import { CreateCourseDto, ReadCourseDto } from './dto';
+import { plainToClass } from 'class-transformer';
+import { Course } from './course.entity';
+import { CourseCalendar } from '../calendar/course-calendar.entity';
+import { CreateCourseCalendarDto } from '../calendar/dto';
+import { Level } from '../level/level.entity';
+import { CourseStudent } from './course-student.entity';
+import { CourseInstructor } from './course-instructor.entity';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(CourseRepository)
+    private readonly _courseRepository: CourseRepository,
+    @InjectRepository(CourseRepository)
+    private readonly _calendarRepository: CourseRepository,
+  ) {}
+  async get(courseId: number): Promise<ReadCourseDto> {
+    if (!courseId) {
+      throw new BadRequestException('courseId must be sent');
+    }
+    const course: Course = await this._courseRepository.findOne(courseId);
+    if (!course) {
+      throw new HttpException('This course does not exists', HttpStatus.OK);
+    }
+    return plainToClass(ReadCourseDto, course);
+  }
+
+  async getAll(): Promise<ReadCourseDto[]> {
+    const courses: Course[] = await this._courseRepository.find();
+
+    if (!courses) {
+      throw new NotFoundException();
+    }
+
+    return courses.map((course: Course) => plainToClass(ReadCourseDto, course));
+  }
+  async create(course: any, calendar: any): Promise<ReadCourseDto> {
+    const savedCourse = await this._courseRepository
+      .create({
+        level: course.levelId,
+        sport: course.sportId,
+        type: course.typeId,
+      })
+      .save();
+    calendar.courseId = savedCourse.id;
+    await CourseCalendar.save(calendar);
+
+    let courses = [];
+    for (let i = 0; i < course.studentId.length; i++) {
+      courses.push({
+        courseId: savedCourse.id,
+        studentId: course.studentId[i],
+      });
+    }
+
+    await CourseStudent.createQueryBuilder()
+      .insert()
+      .into(CourseStudent)
+      .values(courses)
+      .execute();
+
+    await CourseInstructor.createQueryBuilder()
+      .insert()
+      .into(CourseInstructor)
+      .values({ courseId: savedCourse.id, instructorId: course.instructorId })
+      .execute();
+
+    return plainToClass(ReadCourseDto, savedCourse);
+  }
+
+  async studentPaid(
+    courseId: number,
+    studentId: number,
+  ): Promise<ReadCourseDto> {
+    const course = await CourseStudent.findOne({
+      where: {
+        courseId,
+        studentId,
+      },
+    });
+    course.paid = true;
+    course.save();
+    return plainToClass(Course, course);
+  }
+
+  async instructorCashed(
+    courseId: number,
+    instructorId: number,
+  ): Promise<ReadCourseDto> {
+    const course = await CourseInstructor.findOne({
+      where: {
+        courseId,
+        instructorId,
+      },
+    });
+    course.cashed = true;
+    course.save();
+    return plainToClass(Course, course);
+  }
+}
