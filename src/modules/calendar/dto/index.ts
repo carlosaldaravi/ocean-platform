0825aecebfa58b2619716321b772dd684712c29a
Carlos <carlosaldaravi@gmail.com>
2020-05-03 05:04:45 +0200

    [ADD] dto to index

--- src/modules/calendar/dto/index.ts
+++ src/modules/calendar/dto/index.ts
@@ -1,3 +1,4 @@
 export { CreateUserCalendarDto } from './create-user-calendar.dto';
 export { UpdateUserCalendarDto } from './update-user-calendar.dto';
 export { ReadUserCalendarDto } from './read-user-calendar.dto';
+export { CreateCourseCalendarDto } from './create-course-calendar.dto';
