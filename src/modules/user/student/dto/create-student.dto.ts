24dfac97b44ebd0a201887f84be63717a82a31c9
Carlos <carlosaldaravi@gmail.com>
2020-05-06 03:27:26 +0200

    [ADD] languages to create student dto

--- src/modules/user/student/dto/create-student.dto.ts
+++ src/modules/user/student/dto/create-student.dto.ts
@@ -3,6 +3,7 @@ import { Sport } from 'src/modules/sport/sport.entity';
 import { Level } from 'src/modules/level/level.entity';
 import { UserCalendar } from 'src/modules/calendar/user-calendar.entity';
 import { UserDetails } from '../../user.details.entity';
+import { Language } from 'src/modules/language/language.entity';
 
 export class CreateStudentDto {
   @Type(type => UserDetails)
@@ -13,6 +14,9 @@ export class CreateStudentDto {
 
   @Type(type => Level)
   level: Level;
+
+  @Type(type => Language)
+  languages: Language[];
 
   @Type(type => UserCalendar)
   calendar: UserCalendar[];
