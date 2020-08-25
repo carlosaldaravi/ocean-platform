94065b2851bfc2b2b8ee0d69107fa42d36799361
Carlos <carlosaldaravi@gmail.com>
2020-08-25 16:51:17 +0200

    [ADD] json url encode

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { Logger } from '@nestjs/common';
 import { setDefaultValues } from './database/data/create';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerOptions } from './config/config.swagger';
+import { json, urlencoded } from 'express';
 
 dotenv.config();
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('api', app, document);
